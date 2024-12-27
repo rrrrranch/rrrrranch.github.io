29ebdde99a72fff76411259e94eed7ff13a81097
GonzaloNediani <166745356+gonza6g@users.noreply.github.com>
2024-12-27 15:15:36 -0300

    Enhance Navbar component by adding target="_blank" to social and navbar links for improved user experience. This change ensures that external links open in a new tab.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,18 +6,18 @@ const Navbar: React.FC = () => {
         <nav className="navbar">
             <div className="navbar-container">
                 <h1 className="navbar-title">Gonzalo Nediani</h1>
-                <a className="social-link" href="https://x.com/rrrrranch">X</a>
-                <a className="navbar-link" href="#home">#</a>
-                <a className="social-link" href="https://github.com/rrrrranch">GHUB</a>
-                <a className="navbar-link" href="#home">?</a>
-                <a className="navbar-link" href="#about">MORE</a>
-                <a className="social-link" href="https://www.linkedin.com/in/gonzalonediani/">LNKD</a>
-                <a className="social-link" href="https://www.youtube.com/@rrrrranch">YOUT</a>
-                <a className="navbar-link" href="#connect">CNNCT</a>
-                <a className="social-link" href="https://www.instagram.com/rrrrranch">INST</a>
-                <a className="navbar-link" href="#projects">PRJ</a>
-                <a className="social-link" href="https://www.behance.net/gonzalonediani">BHNC</a>
-                <a className="navbar-link" href="#home">~</a>
+                <a className="social-link" target="_blank" href="https://x.com/rrrrranch">X</a>
+                <a className="navbar-link" target="_blank" href="#home">#</a>
+                <a className="social-link" target="_blank" href="https://github.com/rrrrranch">GHUB</a>
+                <a className="navbar-link" target="_blank" href="#home">?</a>
+                <a className="navbar-link" target="_blank" href="#about">MORE</a>
+                <a className="social-link" target="_blank" href="https://www.linkedin.com/in/gonzalonediani/">LNKD</a>
+                <a className="social-link" target="_blank" href="https://www.youtube.com/@rrrrranch">YOUT</a>
+                <a className="navbar-link" target="_blank" href="#connect">CNNCT</a>
+                <a className="social-link" target="_blank" href="https://www.instagram.com/rrrrranch">INST</a>
+                <a className="navbar-link" target="_blank" href="#projects">PRJ</a>
+                <a className="social-link" target="_blank" href="https://www.behance.net/gonzalonediani">BHNC</a>
+                <a className="navbar-link" target="_blank" href="#home">~</a>
                 <h2 className="navbar-description">Raised offline, present online.</h2>
             </div>
         </nav>
